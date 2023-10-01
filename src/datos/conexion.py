09a046a8961b7d9a552d450be5b12050e47c5ee0
Luisfemocha <luisfemocha@gmail.com>
2023-09-30 20:18:54 -0500

    Prueba login y vistas
    Se comienza con la estructuración de las funciones que hacen parte del controlador.
    
    Se estructura la forma como se guardarán de forma temporal los usuarios para realizar pruebas.
    
    Se agregan 2 nuevas vistas, formularios y extras.
    Formularios como el ingreso y registro.
    Extras como modales y otras vistas que se requieran (pendientes).
    
    Se agrega al inicio los import necesarios para conectarse a las demás partes del código y se parcha temporalmente el error de matplotlib.
    
    Se modifica requirements.txt para probar el parche de matplotlib.
    
    Co-Authored-By: Daniel Garzon <142264577+Dgarzonac9@users.noreply.github.com>

--- src/datos/conexion.py
+++ src/datos/conexion.py
@@ -1,1 +1,19 @@
-# En este archivo se realizara la lectura de los dataset para amnejarlos en control
+# En este archivo se realizara la lectura de los dataset para manejarlos en control
+
+#class conexion: def __init__(self):
+
+def get_cuentas():
+    cuentas= {
+        'Daniel': 'asddfg654654',
+        'Jean': '321_123asd',
+        'Luis': '123clave_321',
+        'admin': 'grupo18_ppi'
+    }
+
+def check_cuenta(usn, pas):
+    # TODO
+    print('se revisa si existe y coincide')
+
+def registrar_cuenta(usn, pas):
+    # TODO
+    print('se guarda la nueva cuenta')
